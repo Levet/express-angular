76047a0437a3efad237a9cf632cd34a133c21ee8
jordan <jordan@ascensionengineer.com>
2022-11-13 19:20:07 -0700

    Adding angular

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const tasks = require('./routes/tasks');
 const port = 3000;
+const fs = require('fs');
+
+const index = fs.readFileSync('./dist/express-angular/index.html', 'utf8');
 
 tasks(app);
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello'));
+app.use(express.static(path.join(__dirname, "../dist/express-angular")));
+
+app.get('/', (req, res) => {
+    res.header('Content-Type', 'text/html');
+    res.send(index);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
